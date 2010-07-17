6981b88aa89d4ab6609a3df048bf023332e269e8
acampbell3000 <acampbell3000@googlemail.com>
2010-07-17 16:47:41 +0100

    Typo removal

--- oauth-plugin/grails-app/conf/BuildConfig.groovy
+++ oauth-plugin/grails-app/conf/BuildConfig.groovy
@@ -1,3 +1,4 @@
+
 grails.project.dependency.resolution = {
     inherits "global" // inherit Grails' default dependencies
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
